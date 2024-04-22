b2381725826b18a45f9f3e12c74934de9c1b2461
Ninad Shingare <nvshingare67@gmail.com>
2024-04-23 00:29:46 +0530

    final

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -8,22 +8,22 @@ import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfilePage = async ({ searchParams}: SearchParamProps) => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
 
-    const { sessionClaims } = auth();
-    const userId = sessionClaims?.userId as string;
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-    const ordersPage = Number(searchParams?.orderPage) || 1;
-    const eventsPage = Number(searchParams?.orderPage) || 1;
+  const orders = await getOrdersByUser({ userId, page: ordersPage})
 
-    const orders = await getOrdersByUser({ userId, page:ordersPage })
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
-    const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
-    const organizedEvents = await getEventsByUser({ userId, page: eventsPage})
   return (
     <>
-        {/* my tickets */}
-        <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      {/* My Tickets */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
@@ -47,10 +47,8 @@ const ProfilePage = async ({ searchParams}: SearchParamProps) => {
         />
       </section>
 
-
-        {/* events organized */}
-
-        <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      {/* Events Organized */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
